4a124e9388da96b2fb14ebcc2b1244319379b162
jsc1014 <93861470+jsc1014@users.noreply.github.com>
2023-10-04 19:39:57 +0900

    Update BJ_1005_ACM_Craft.java

--- week8/BJ_1005_ACM_Craft.java
+++ week8/BJ_1005_ACM_Craft.java
@@ -1,18 +1,18 @@
-package boj;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BJ_1005_ACM_Craft {
-	
+
 	static int[] inDegree, d;
 	static int goal, n, k;
-	static int[][] map;
+	static ArrayList<Integer>[] list;
+	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -28,31 +28,50 @@ public class BJ_1005_ACM_Craft {
             d = new int[n+1];
             for(int i = 1; i <= n; i++) d[i] = Integer.parseInt(st.nextToken());
             
-            // 인접행렬 - 연결시켜주기
-            map = new int[n+1][n+1];
+            // 진입차수 체크배열
+            inDegree = new int[n+1];
+            
+            // 인접리스트 - 연결시켜주기
+            list = new ArrayList[n+1];
+            for(int i = 1 ; i <= n ; i++) list[i] = new ArrayList<Integer>();
             for(int i = 0 ; i < k ; i++) {
                 st = new StringTokenizer(br.readLine());
                 int from = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
-                map[from][to] = map[to][from] = 1;
+                list[from].add(to);
+                inDegree[to]++;
             }
             
             goal = Integer.parseInt(br.readLine());
-            ans = 0;
-            beforeNode(goal);
-            
+            sol();
+            System.out.println(sum[goal]);
         }
     }
     
-    static int ans, max;
-    static void beforeNode(int node) {
-    	int max = 0;
-    	for(int i = 1; i <= n ; i++) {
-    		// 연결 되어있으면 재귀
-    		if(map[node][i] == 1) {
-    			beforeNode(i);
-    			max = Math.max(max, d[i]);
-    		}
-    	} 
+    static int[] sum;
+    static void sol() {
+    	// 진입차수 0인 노드들 큐에 넣기
+    	Queue<Integer> q = new LinkedList<Integer>();
+    	sum = new int[n+1];
+    	
+        for(int i = 1; i <= n; i++) {
+            if(inDegree[i] == 0) {
+            	q.add(i);
+            }
+        }
+    	
+        // 각 노드별 누적 시간을 저장하는 배열
+        sum = Arrays.copyOfRange(d, 0, d.length);
+    	while(!q.isEmpty()) {
+            
+    		int nowNode = q.poll();
+            for(int to : list[nowNode]) {
+            	sum[to] = Math.max(sum[to], sum[nowNode] + d[to]);
+        		inDegree[to]--;
+        		if(inDegree[to] == 0) {
+        			q.add(to);
+        		}
+        	}
+        }
     }
 }
